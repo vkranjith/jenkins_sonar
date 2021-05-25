053aa90bf7aebf159b4c15ea15f4033297cb515d
Ranjith VK <ranjith_vk@yahoo.com>
2021-05-25 13:01:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,4 +3,4 @@
 # @author Ranjith V K
 # @email ranjith_vk@yahoo.com
 
-bin/magento setup:install --backend-frontname="manager" --db-host="localhost" --db-name="magento" --db-user="magento" --db-password="ranjith@123" --base-url="http://magento.localhost.com" --use-rewrites="1" --admin-user="ranjith" --admin-password="ranjith@123" --admin-email="ranjith_vk@yahoo.com" --admin-firstname="Ranjith" --admin-lastname="V K" --search-engine="elasticsearch7" --elasticsearch-host="localhost" --elasticsearch-port="9200"
+bin/magento setup:install --backend-frontname="manager" --db-host="localhost" --db-name="magento" --db-user="magento" --db-password="ranjith@123" --base-url="http://magento.localhost.com/" --use-rewrites="1" --admin-user="ranjith" --admin-password="ranjith@123" --admin-email="ranjith_vk@yahoo.com" --admin-firstname="Ranjith" --admin-lastname="V K" --search-engine="elasticsearch7" --elasticsearch-host="localhost" --elasticsearch-port="9200"
