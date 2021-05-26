d0a088fa309c11292ede9effed69fe41720cac36
Ranjith VK <ranjith_vk@yahoo.com>
2021-05-26 22:17:10 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,9 +4,69 @@
 
 echo "Starting the Build Process..."
 
+# Set any variables
 ENV_FILE=app/etc/env.php
 
-pwd
+# Check the variables and set default values if necessary
+if [ ! $ADMIN_URL ]; then
+    # generate random admin url frontname
+    ADMIN_URL=manage_$(date +%s | sha256sum | base64 | head -c 8)
+fi
+
+if [ ! $DATABASE_HOST ]; then
+    DATABASE_HOST=localhost:3306
+fi
+
+if [ ! $DATABASE_NAME ]; then
+    DATABASE_NAME=magento
+fi
+
+if [ ! $DATABASE_USERNAME ]; then
+    DATABASE_USERNAME=magento
+fi
+
+if [ ! $DATABASE_PASSWORD ]; then
+    DATABASE_PASSWORD=''
+fi
+
+if [ ! $APP_URL ]; then
+    APP_URL=http://localhost/
+fi
+
+if [ ! $ADMIN_USER ]; then
+    ADMIN_USER=admin_$(date +%s | sha256sum | base64 | head -c 8)
+fi
+
+if [ ! $ADMIN_PASSWORD ]; then
+    ADMIN_PASSWORD=date +%s | sha256sum | base64 | head -c 16
+fi
+
+if [ ! $ADMIN_FNAME ]; then
+    ADMIN_FNAME=admin
+fi
+
+if [ ! $ADMIN_LNAME ]; then
+    ADMIN_LNAME=admin
+fi
+
+if [ ! $ELASTICSEARCH_VERSION ]; then
+    ELASTICSEARCH_VERSION=elasticsearch7
+fi
+
+if [ ! $ELASTICSEARCH_HOST ]; then
+    ELASTICSEARCH_HOST=localhost
+fi
+
+if [ ! $ELASTICSEARCH_PORT ]; then
+    ELASTICSEARCH_PORT=9200
+fi
+
+if [ ! $DEPLOY_MODE ]; then
+    DEPLOY_MODE=developer
+fi
+
+
+# clean up the vendor files and pull a fresh copy
 rm -rf vendor/*
 composer install
 # chown -R jenkins:jenkins ./
@@ -24,17 +84,28 @@ rm -rf var/report/*
 
 # Check if the env.php file exists and if not, install the app
 if [ ! -f $ENV_FILE ]; then
-  bin/magento setup:install --backend-frontname="manager" --db-host="localhost" --db-name="magento" --db-user="magento" --db-password="ranjith@123" --base-url="http://magento.localhost.com/" --use-rewrites="1" --admin-user="ranjith" --admin-password="ranjith@123" --admin-email="ranjith_vk@yahoo.com" --admin-firstname="Ranjith" --admin-lastname="V K" --search-engine="elasticsearch7" --elasticsearch-host="localhost" --elasticsearch-port="9200"
+  bin/magento setup:install --backend-frontname="$ADMIN_URL" \
+    --db-host="$DATABASE_HOST" \
+    --db-name="$DATABASE_NAME" \
+    --db-user="$DATABASE_USERNAME" \
+    --db-password="$DATABASE_PASSWORD" \
+    --base-url="$APP_URL" \
+    --use-rewrites="1" \
+    --admin-user="$ADMIN_USER" \
+    --admin-password="$ADMIN_PASSWORD" \
+    --admin-email="$ADMIN_EMAIL" \
+    --admin-firstname="$ADMIN_FNAME" \
+    --admin-lastname="$ADMIN_LNAME" \
+    --search-engine="$ELASTICSEARCH_VERSION" \
+    --elasticsearch-host="$ELASTICSEARCH_HOST" \
+    --elasticsearch-port="$ELASTICSEARCH_PORT"
 fi
 
 # deploy and compile
 bin/magento app:config:import
 bin/magento setup:upgrade
-bin/magento deploy:mode:set developer
+bin/magento deploy:mode:set $DEPLOY_MODE
 bin/magento setup:di:compile
 bin/magento setup:static-content:deploy -f
 
-
-
 echo "Completed the Build Process..."
-
