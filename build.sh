b09ebe392fef3964d7c00f74844a9cba5abb802e
Ranjith VK <ranjith_vk@yahoo.com>
2021-05-27 18:11:16 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-echo "Starting the Build Process..."
-
 # Set any variables
 ENV_FILE=app/etc/env.php
 
@@ -75,68 +73,84 @@ if [ ! -d $SERVER_LOCATION ]; then
     set SERVER_LOCATION=~/home/$FILE_OWNER/public_html/
 fi
 
-echo "Location: $SERVER_LOCATION"
-
 # remove trailing forward slash "/" from the path and add ".tmp"
 SERVER_BUILD_LOCATION=$(echo $SERVER_LOCATION | sed 's/\/$//g').tmp
 
-echo "Server Location..."
-echo $SERVER_BUILD_LOCATION
 
 ssh $FILE_OWNER@$SERVER_ADDRESS << EOF
+    if [ $STAGE_NAME == "Build" ]; then
+        echo "Starting the Build Process..."
 
-cd $SERVER_BUILD_LOCATION
-echo "Current directory for build:"
-pwd
+        cd $SERVER_BUILD_LOCATION
+        echo "Current directory for build:"
+        pwd
 
-# clean and pull the latest code
-git reset --hard HEAD
-# assuming the current branch is the correct branch to build and deploy
-git pull 
+        # clean and pull the latest code
+        git reset --hard HEAD
+        # assuming the current branch is the correct branch to build and deploy
+        git pull
 
-# clean up the vendor files and pull a fresh copy
-rm -rf vendor/*
-composer install
-chown -R $FILE_OWNER:$SERVER_USER ./
+        # clean up the vendor files and pull a fresh copy
+        rm -rf vendor/*
+        composer install
+        chown -R $FILE_OWNER:$SERVER_USER ./
 
-# set appropriate permissions for the build
-chmod +x bin/magento
+        # set appropriate permissions for the build
+        chmod +x bin/magento
 
-# clean the previously generated deployment and compiled files
-rm -r var/view_preprocessed/*
-rm -r pub/static/*/*
-rm -r generated/*/*
-rm -r var/log/*
-rm -rf var/report/*
+        # clean the previously generated deployment and compiled files
+        rm -r var/view_preprocessed/*
+        rm -r pub/static/*/*
+        rm -r generated/*/*
+        rm -r var/log/*
+        rm -rf var/report/*
 
-# Check if the env.php file exists and if not, install the app
-if [ ! -f $ENV_FILE ]; then
-  bin/magento setup:install \
-    --backend-frontname="$ADMIN_URL" \
-    --db-host="$DATABASE_HOST" \
-    --db-name="$DATABASE_NAME" \
-    --db-user="$DATABASE_USERNAME" \
-    --db-password="$DATABASE_PASSWORD" \
-    --base-url="$APP_URL" \
-    --use-rewrites="1" \
-    --admin-user="$ADMIN_USER" \
-    --admin-password="$ADMIN_PASSWORD" \
-    --admin-email="$ADMIN_EMAIL" \
-    --admin-firstname="$ADMIN_FNAME" \
-    --admin-lastname="$ADMIN_LNAME" \
-    --search-engine="$ELASTICSEARCH_VERSION" \
-    --elasticsearch-host="$ELASTICSEARCH_HOST" \
-    --elasticsearch-port="$ELASTICSEARCH_PORT"
-fi
+        # Check if the env.php file exists and if not, install the app
+        if [ ! -f $ENV_FILE ]; then
+          bin/magento setup:install \
+            --backend-frontname="$ADMIN_URL" \
+            --db-host="$DATABASE_HOST" \
+            --db-name="$DATABASE_NAME" \
+            --db-user="$DATABASE_USERNAME" \
+            --db-password="$DATABASE_PASSWORD" \
+            --base-url="$APP_URL" \
+            --use-rewrites="1" \
+            --admin-user="$ADMIN_USER" \
+            --admin-password="$ADMIN_PASSWORD" \
+            --admin-email="$ADMIN_EMAIL" \
+            --admin-firstname="$ADMIN_FNAME" \
+            --admin-lastname="$ADMIN_LNAME" \
+            --search-engine="$ELASTICSEARCH_VERSION" \
+            --elasticsearch-host="$ELASTICSEARCH_HOST" \
+            --elasticsearch-port="$ELASTICSEARCH_PORT"
+        fi
 
-# deploy and compile
-bin/magento app:config:import
-bin/magento setup:upgrade
-bin/magento deploy:mode:set developer
-bin/magento setup:di:compile
-bin/magento setup:static-content:deploy -f
-bin/magento deploy:mode:set $DEPLOY_MODE
+        # deploy and compile
+        bin/magento app:config:import
+        bin/magento setup:upgrade
+        bin/magento deploy:mode:set developer
+        bin/magento setup:di:compile
+        bin/magento setup:static-content:deploy -f
+        bin/magento deploy:mode:set $DEPLOY_MODE
+
+        echo "Completed the Build Process..."
+    fi
+
+    if [ $STAGE_NAME == "Deploy" ]; then
+        echo "Deploying the build files..."
+
+        cd $SERVER_LOCATION
+        bin/magento maintenance:enable
+
+        # sync the build files
+        rsync -rv $SERVER_BUILD_LOCATION/* $SERVER_LOCATION
+
+        # remove all GIT files
+        rm -rf .git/
+        bin/magento set:up
+        bin/magento maintenance:disable
+
+        echo "Deployment complete";
+    fi
 
 EOF
-
-echo "Completed the Build Process..."
